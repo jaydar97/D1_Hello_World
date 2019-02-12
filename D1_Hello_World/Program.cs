215658634d02d25116acfdc6f86a6e00f63af8d2
jayda.rance@gmail.com <jayda.rance@gmail.com>
2019-02-12 15:09:53 -0500

    Add project files.

---
+++ D1_Hello_World/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace D1_Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
